d6f79e793f491d67389720d3218eeb2fbf3436bd
Delorean <htcheroportugal@gmail.com>
2025-05-15 23:06:37 +0100

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,7 +1,6 @@
 
 const map = L.map('map').setView([39.5, -8.0], 7);
 
-
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
